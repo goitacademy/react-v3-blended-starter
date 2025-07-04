ad64dbe9caa62824b07f6c92f2eb406ad0cb70b3
Artur_M <64428009+ArturMykhalyk@users.noreply.github.com>
2025-07-04 12:24:55 +0300

    Push

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,7 +1,20 @@
 import Section from "../Section/Section";
 import Container from "../Container/Container";
+import { getPhotos } from "../../services/photos";
+import { useEffect } from "react";
 
 export default function App() {
+  const query: string = "cat";
+
+  useEffect(() => {
+    const fetchPhotos = async () => {
+      const data = await getPhotos(query);
+      console.log(data);
+    };
+
+    fetchPhotos();
+  }, [query]);
+
   return (
     <>
       <Section>
