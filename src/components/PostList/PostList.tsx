527613d4a7912921bb400714eac13bfce70672fc
Ann K <kubareva.ann@gmail.com>
2025-07-19 18:50:34 +0300

    practice

--- src/components/PostList/PostList.tsx
+++ src/components/PostList/PostList.tsx
@@ -1,17 +1,36 @@
-import css from "./PostList.module.css";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { Post } from "../../types/post";
 
-export default function PostList() {
+import css from "./PostList.module.css";
+import { deletePost } from "../../services/postService";
+
+interface PostListProps {
+  posts: Post[];
+}
+
+export default function PostList({ posts }: PostListProps) {
+  const queryClient = useQueryClient();
+  const mutation = useMutation({
+    mutationFn: deletePost,
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ["post"] });
+      alert("post deleted");
+    },
+  });
   return (
     <ul className={css.list}>
-      {/* список постів, кожен з яких створює наступну розмітку */}
-      <li className={css.listItem}>
-        <h2 className={css.title}>Title</h2>
-        <p className={css.content}>Контент</p>
-        <div className={css.footer}>
-          <button className={css.edit}>Edit</button>
-          <button className={css.delete}>Delete</button>
-        </div>
-      </li>
+      {posts.map((post) => (
+        <li className={css.listItem} key={post.id}>
+          <h2 className={css.title}>{post.title}</h2>
+          <p className={css.content}>{post.body}</p>
+          <div className={css.footer}>
+            <button className={css.edit}>Edit</button>
+            <button className={css.delete} onClick={() => mutation.mutate(post.id)}>
+              Delete
+            </button>
+          </div>
+        </li>
+      ))}
     </ul>
   );
 }
