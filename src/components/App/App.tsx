3574c4e4a8d03f203a4f6a435683d2e3ecccecc2
Anna Balesko <babasheldona@icloud.com>
2026-01-24 12:06:36 +0100

    add modal

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,13 +1,68 @@
 import Section from "../Section/Section";
 import Container from "../Container/Container";
+import Form from "../Form/Form";
+import toast, { Toaster } from "react-hot-toast";
+import { useState } from "react";
+import type { Photo } from "../../types/photo";
+import { getPhotos } from "../../services/photos";
+import Loader from "../Loader/Loader";
+import Text from "../Text/Text";
+import PhotosGallery from "../PhotosGallery/PhotosGallery";
+import Modal from "../Modal/Modal";
 
 export default function App() {
+  const [selectedPhoto, setSelectedPhoto] = useState<Photo | null>(null);
+  const [photos, setPhotos] = useState<Photo[]>([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [isError, setIsError] = useState(false);
+
+  const handleSubmit = async (query: string) => {
+    try {
+      setIsError(false);
+      setIsLoading(true);
+      setPhotos([]);
+      const result = await getPhotos(query);
+      if (result.length === 0) {
+        toast.error("No found photos");
+        return;
+      }
+      setPhotos(result);
+    } catch {
+      setIsError(true);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  const handleSelecdedPhoto = (photo: Photo | null) => {
+    setSelectedPhoto(photo);
+  };
+
   return (
     <>
       <Section>
-        <Container>{/* Компоненти застосунку */}</Container>
-        Home page
+        <Container>
+          <Form onSubmit={handleSubmit} />
+          {isLoading && <Loader />}
+          {isError && <Text textAlign="center">Somesing went wrong</Text>}
+          {photos.length > 0 && (
+            <PhotosGallery selectPhoto={handleSelecdedPhoto} photos={photos} />
+          )}
+          {selectedPhoto && (
+            <Modal onClose={() => handleSelecdedPhoto(null)}>
+              <div
+                style={{
+                  backgroundColor: selectedPhoto.avg_color,
+                  borderColor: selectedPhoto.avg_color,
+                }}
+              >
+                <img src={selectedPhoto.src.large} alt={selectedPhoto.alt} />
+              </div>
+            </Modal>
+          )}
+        </Container>
       </Section>
+      <Toaster />
     </>
   );
 }
--- src/components/Modal/Modal.tsx
+++ src/components/Modal/Modal.tsx
@@ -1,14 +1,51 @@
+import { createPortal } from "react-dom";
 import styled from "./Modal.module.css";
+import { useEffect, type ReactNode } from "react";
 
-export default function Modal() {
-  return (
-    <div className={styled.backdrop} role="dialog" aria-modal="true">
+interface ModalProps {
+  children: ReactNode;
+  onClose: () => void;
+}
+
+export default function Modal({ children, onClose }: ModalProps) {
+  useEffect(() => {
+    const handleKeyDown = (e: KeyboardEvent) => {
+      if (e.key === "Escape") {
+        onClose();
+      }
+    };
+    document.addEventListener("keydown", handleKeyDown);
+    document.body.style.overflow = "hidden";
+
+    return () => {
+      document.removeEventListener("keydown", handleKeyDown);
+      document.body.style.overflow = "";
+    };
+  }, [onClose]);
+
+  const handleBackdropClick = (e: React.MouseEvent<HTMLDivElement>) => {
+    if (e.target === e.currentTarget) {
+      onClose();
+    }
+  };
+  return createPortal(
+    <div
+      className={styled.backdrop}
+      role="dialog"
+      aria-modal="true"
+      onClick={handleBackdropClick}
+    >
       <div className={styled.modal}>
-        <button className={styled.closeButton} aria-label="Close modal">
+        <button
+          className={styled.closeButton}
+          aria-label="Close modal"
+          onClick={onClose}
+        >
           &times;
         </button>
-        {/* children */}
+        {children}
       </div>
-    </div>
+    </div>,
+    document.body,
   );
 }
